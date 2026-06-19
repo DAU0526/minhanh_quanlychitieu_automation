26448e0843901cfa857c7717c31ce744083f5836
Minh Anh <minhanh15104@gmail.com>
2026-06-19 14:36:17 +0700

    Update locators for login button and register link

--- resources/locators/LoginPageLocators.py
+++ resources/locators/LoginPageLocators.py
@@ -1,3 +1,7 @@
 EMAIL_INPUT = "id=email"
+
 PASSWORD_INPUT = "id=password"
-LOGIN_BUTTON = "id=login"
+
+LOGIN_BUTTON = "xpath=//button[contains(text(),'Đăng nhập')]"
+
+REGISTER_LINK = "xpath=//a[contains(text(),'Tạo tài khoản mới')]"
