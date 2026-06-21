c8c0a413d9ffb39484427dfc02dc38a8b2311f49
Minh Anh <minhanh15104@gmail.com>
2026-06-21 19:16:11 +0700

    Update TransactionPageLocators.py

--- resources/locators/TransactionPageLocators.py
+++ resources/locators/TransactionPageLocators.py
@@ -1,6 +1,22 @@
-NAME_INPUT = "id=name"
-AMOUNT_INPUT = "id=amount"
-TYPE_DROPDOWN = "id=type"
-CATEGORY_DROPDOWN = "id=category"
-SAVE_BUTTON = "id=save"
-DELETE_BUTTON = "id=delete"
+TRANSACTION_NAME_INPUT = "xpath=(//input[@type='text'])[1]"
+
+AMOUNT_INPUT = "xpath=(//input[@type='number'])[1]"
+
+TYPE_DROPDOWN = "xpath=(//select)[1]"
+
+CATEGORY_DROPDOWN = "xpath=(//select)[2]"
+
+SAVE_BUTTON = "css=button.btn-primary"
+
+DOWNLOAD_SAMPLE_BUTTON = "xpath=//a[contains(text(),'Tải tệp mẫu')]"
+
+IMPORT_CSV_BUTTON = "xpath=//label[contains(.,'Nhập CSV')]"
+
+NAME_INPUT = "xpath=//input[@placeholder='VD: Tiền ăn trưa']"
+
+AMOUNT_INPUT = "xpath=//input[@placeholder='VD: 50000']"
+
+SAVE_BUTTON = "css=button.btn-primary"
+
+TRANSACTION_HISTORY = "xpath=//*[contains(text(),'Lịch sử giao dịch')]"
+
