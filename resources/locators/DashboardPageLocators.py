a504bd44ef75713d22db198ca06c636cde7d21b2
Minh Anh <minhanh15104@gmail.com>
2026-06-21 19:15:47 +0700

    Add locators for dashboard elements in DashboardPageLocators

--- resources/locators/DashboardPageLocators.py
+++ resources/locators/DashboardPageLocators.py
@@ -1,3 +1,13 @@
-BALANCE_LABEL = "id=balance"
-INCOME_LABEL = "id=income"
-EXPENSE_LABEL = "id=expense"
+STATISTICS_TAB = "xpath=//*[contains(text(),'Thống kê')]"
+
+TRANSACTION_TAB = "xpath=//*[contains(text(),'Giao dịch')]"
+
+WELCOME_TEXT = "xpath=//*[contains(text(),'Chào')]"
+
+BALANCE_CARD = "xpath=//*[contains(text(),'SỐ DƯ HIỆN TẠI')]"
+
+INCOME_CARD = "xpath=//*[contains(text(),'TỔNG THU NHẬP')]"
+
+EXPENSE_CARD = "xpath=//*[contains(text(),'TỔNG CHI TIÊU')]"
+
+LOGOUT_BUTTON = "xpath=//button[contains(text(),'Đăng xuất')]"
