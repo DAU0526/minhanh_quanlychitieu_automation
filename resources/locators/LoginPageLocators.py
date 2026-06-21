8b2294ca24363b700800733ffcdc1b3abd6362d7
Minh Anh <minhanh15104@gmail.com>
2026-06-21 19:15:14 +0700

    Update LoginPageLocators.py

--- resources/locators/LoginPageLocators.py
+++ resources/locators/LoginPageLocators.py
@@ -2,3 +2,4 @@ EMAIL_INPUT = "xpath=(//input[@type='email'])[1]"
 PASSWORD_INPUT = "xpath=(//input[@type='password'])[1]"
 LOGIN_BUTTON = "xpath=//button[contains(text(),'Đăng nhập')]"
 REGISTER_LINK = "xpath=//a[contains(text(),'Tạo tài khoản mới')]"
+
